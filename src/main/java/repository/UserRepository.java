c57f8a943e23d6795db5a3b003721953712e038c
omarAlbaarah <64056063+omarAlbaarah@users.noreply.github.com>
2020-05-11 05:58:13 -0500

    Omar userControll first update

--- src/main/java/repository/UserRepository.java
+++ src/main/java/repository/UserRepository.java
@@ -1,11 +1,44 @@
 package repository;
 
+import models.User;
+
 import java.io.Serializable;
+import java.util.HashMap;
 
 public class UserRepository implements Serializable {
 
-    public UserRepository(){
+    // private User user;
+    private HashMap<String, User> users;
 
+    public UserRepository(){
+        users = new HashMap<>();
+        CreateUsers();
+    }
+
+    private void CreateUsers() {
+        users.put("omar",new User("omar","12345","abc123"));
+        users.put("ahmad",new User("ahmad","67890","a1234"));
+    }
+
+    //get account number for rendering page
+    public String getAccountNUmber(String username) {
+        return getUserByUsername(username).getAccountNumber();
+    }
+
+    public void addUser(User user) {
+        users.put(user.getUserName(),user);
+    }
+
+    public User getUserByUsername(String username){
+        return users.get(username);
+    }
+
+    public void deleteUser(String username){
+        users.remove(username);
+    }
+
+    public HashMap<String,User> getUsers(){
+        return users;
     }
 
 }
